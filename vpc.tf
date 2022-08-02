833a89eb818aefbeed78d0142fe5c3fb8840422b
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-08-02 22:36:15 +0530

    restructure vpc

--- vpc.tf
+++ vpc.tf
@@ -1,59 +1,86 @@
 resource "ibm_is_vpc" "vmw" {
   resource_group = ibm_resource_group.VMware.id
-  name = "vmw"
+  name           = "vmw"
 }
 
 resource "ibm_is_vpc_address_prefix" "vmware-vpc-prefix" {
   name = "vmware-vpc-prefix"
   zone = var.zone
   vpc  = ibm_is_vpc.vmw.id
-  cidr = "10.97.0.0/22"
+  cidr = "10.1.0.0/16"
 }
 
-resource "ibm_is_subnet" "vmw-host-mgmt-subnet" {
-  depends_on = [ ibm_is_vpc_address_prefix.vmware-vpc-prefix ]
-  name            = "vmw-host-mgmt-subnet"
+resource "ibm_is_vpc_address_prefix" "apic-subsys-vpc-prefix" {
+  name = "apic-subsys-vpc-prefix"
+  zone = var.zone
+  vpc  = ibm_is_vpc.vmw.id
+  cidr = "172.22.10.0/24"
+}
+
+resource "ibm_is_vpc_address_prefix" "apic-gw-vpc-prefix" {
+  name = "apic-gw-vpc-prefix"
+  zone = var.zone
+  vpc  = ibm_is_vpc.vmw.id
+  cidr = "192.168.70.0/24"
+}
+
+resource "ibm_is_subnet" "vmw-host-subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.vmware-vpc-prefix]
+  name            = "vmw-host-subnet"
   vpc             = ibm_is_vpc.vmw.id
   zone            = var.zone
-  ipv4_cidr_block = "10.97.0.0/25"
+  ipv4_cidr_block = "10.1.1.0/24"
+  resource_group  = ibm_resource_group.VMware.id
 }
 
-resource "ibm_is_subnet" "vmw-inst-mgmt-subnet" {
-  depends_on = [ ibm_is_vpc_address_prefix.vmware-vpc-prefix ]
-  name            = "vmw-inst-mgmt-subnet"
+resource "ibm_is_subnet" "vmw-mgmt-subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.vmware-vpc-prefix]
+  name            = "vmw-mgmt-subnet"
   vpc             = ibm_is_vpc.vmw.id
   zone            = var.zone
-  ipv4_cidr_block = "10.97.0.128/25"
+  ipv4_cidr_block = "10.1.2.0/24"
+  resource_group  = ibm_resource_group.VMware.id
 }
 
-resource "ibm_is_subnet" "vmw-vmot-subnet" {
-  depends_on = [ ibm_is_vpc_address_prefix.vmware-vpc-prefix ]
-  name            = "vmw-vmot-subnet"
+resource "ibm_is_subnet" "vmw-apic-subsys-subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.apic-subsys-vpc-prefix]
+  name            = "vmw-apic-subsys-subnet"
   vpc             = ibm_is_vpc.vmw.id
   zone            = var.zone
-  ipv4_cidr_block = "10.97.1.0/25"
+  ipv4_cidr_block = "192.168.70.0/25"
+  resource_group  = ibm_resource_group.VMware.id
 }
 
-resource "ibm_is_subnet" "vmw-vsan-subnet" {
-  depends_on = [ ibm_is_vpc_address_prefix.vmware-vpc-prefix ]
-  name            = "vmw-vsan-subnet"
+resource "ibm_is_subnet" "vmw-apic-gw-frontend-subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.apic-gw-vpc-prefix]
+  name            = "vmw-apic-gw-frontend-subnet"
   vpc             = ibm_is_vpc.vmw.id
   zone            = var.zone
-  ipv4_cidr_block = "10.97.2.0/25"
+  ipv4_cidr_block = "172.22.10.0/25"
+  resource_group  = ibm_resource_group.VMware.id
 }
 
-resource "ibm_is_public_gateway" "vmware-mgmt-internet-outbound" {
-  name = "vmware-mgmt-internet-outbound"
+resource "ibm_is_subnet" "vmw-apic-gw-backend-subnet" {
+  depends_on      = [ibm_is_vpc_address_prefix.apic-gw-vpc-prefix]
+  name            = "vmw-apic-gw-backend-subnet"
+  vpc             = ibm_is_vpc.vmw.id
+  zone            = var.zone
+  ipv4_cidr_block = "172.22.10.128/25"
+  resource_group  = ibm_resource_group.VMware.id
+}
+
+resource "ibm_is_public_gateway" "vmware-internet-outbound" {
+  name = "vmware-internet-outbound"
   vpc  = ibm_is_vpc.vmw.id
   zone = var.zone
 }
 
-resource "ibm_is_subnet_public_gateway_attachment" "vmware-mgmt-internet-outbound-gw-attachment" {
-  subnet                = ibm_is_subnet.vmw-inst-mgmt-subnet.id
-  public_gateway         = ibm_is_public_gateway.vmware-mgmt-internet-outbound.id
+resource "ibm_is_subnet_public_gateway_attachment" "vmware-internet-outbound-gw-attachment" {
+  subnet         = ibm_is_subnet.vmw-mgmt-subnet.id
+  public_gateway = ibm_is_public_gateway.vmware-internet-outbound.id
 }
 
-resource "ibm_is_ssh_key" "cloud-workstation-1" {
-  name       = "cloud-workstation-1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDDn/kKHVZdz8VHzw6U2evrvgGlUXqVKiN5DABTfDHF7BNq8YIOJRuo31u3LxMLaZvzaWvIC1GFoHWkWsR8cf/KQ1AZ65Z74erBolGTQ0HhxscSTEe3j/RMeWZLsGfb945nmjV1d+jc1iuVqsm+wMQ5oPsIK/UWqhyP/hG3v9LPE11QJaw/GCa2WKyveLavirh7kcOPawwwGf2WDmf2wCn5xHoKY57wlkY9QfY2WTsq+kQ2JhSyHm5Kn+aWKOw0h5gCWfD5Owq3CcUdtzXO30us5xHIi3AlJ9u3lGZdI4+ium76wTQqZaa+lZY0/xDDk4LesqgQCYFlIMpOd5yym4P0kiTdQms3Y47HUZa/iDsGSh8RFuYN86fcJHLODhzZzJrFkjGd/Hg8hbRupnpsF5kay0AiNr6zE/sSIg7rsCjiv/SbPnGtpF72m3Vg8jFCJcsigsYhFrBwAIALkNrmwgxoXgJa0lxkLzQoyhQ5hQgKG8/JpHyensQHjHsAetiYYSAwlV+qXP0ANty9IH9Y7e21utebUpSEAg5Mlgwm70ERPkZTgJ5H6FYxrMaUkCjZaIMBJ+WNjjyVq7uduGb0F/qWA88xl1vz7eGRDHqXkNx3qDxhHkh+55/6/GnvlhtdNDKM6+nOQ+iQnj6M4Y2Y50oLsHUVJQxc4y7HayO9fvDYdw== iresh"
+resource "ibm_is_ssh_key" "iresh-pc" {
+  name       = "iresh-pc"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC64dK7V6GBPV77TzGbsMlEKvZh0earMCCYL7lSseqpLECLVHrO7AjdDjM8jMGezPD4sHGAJN105ZZdu887M4+7KufuUF/CooVJ3e7iH/aL98LZBXxgbsebxvPJael2MULXO6koK26QHrY9FwtVjc1BQWG7MbCaCH3CjhNa+nefEaBhGj+0jnd2eDZjUCTMzFt/H50Ref9tRcAiQCwG9KBJVKMrhiSrjiKcfowVFNbvBunlLFb0Gy79UHP4AVHMl3burUS9cZlU092lBvY+oyTZrKucgm8lGB+d8t9uiMfGGv9uYmka20hPd672N+qa/PdwESXFkd9QoOVkVp3FpqA40TNyeW0DKIk4BOnn3xVzgs0SXyXqF7W8a4rltR0GTqvQ93FC6kdTwlQUhXEjXdUMM/zNJRXALV0Dw/av4QgON18a4UoU2Xd8jxBsQDf7ENlWXdfavFn6KhI/iY3IaOyezfnzlj5qbQBLom7nqFJj1YZW1C6rK51oCf9q2bQPrF0= iresh@iresh-pc"
 }
