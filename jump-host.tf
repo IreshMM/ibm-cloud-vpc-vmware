8b1900607aa500eb4e0dd2ed3754c3c2a7650bc1
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-08-02 23:21:28 +0530

    restructure jump-host for nsb apic setup

--- jump-host.tf
+++ jump-host.tf
@@ -1,35 +1,29 @@
-resource "ibm_is_instance" "jump-001" {
-  name    = "jump-001"
+resource "ibm_is_instance" "jump-host" {
+  name    = "jump-host"
   image   = "r010-eaebade7-d050-4e4d-b552-c45e53c9a893"
-  profile = "bx2-2x8"
+  profile = "cx2-2x4"
 
   primary_network_interface {
-    subnet = ibm_is_subnet.vmw-inst-mgmt-subnet.id
-  }
-
-  network_interfaces {
-    name = "eth1"
-    allow_ip_spoofing = false
-    subnet = ibm_is_subnet.vmw-vmot-subnet.id
+    subnet = ibm_is_subnet.vmw-mgmt-subnet.id
   }
 
   vpc  = ibm_is_vpc.vmw.id
   zone = var.zone
-  keys = [ibm_is_ssh_key.cloud-workstation-1.id, "r010-603b53db-11e3-4d41-83e0-626c219412a7"]
+  keys = [ibm_is_ssh_key.iresh-pc.id]
 }
 
-resource "ibm_is_floating_ip" "jump-001-fip" {
-  name   = "jump-001-fip"
-  target = ibm_is_instance.jump-001.primary_network_interface[0].id
+resource "ibm_is_floating_ip" "jump-host-fip" {
+  name   = "jump-host-fip"
+  target = ibm_is_instance.jump-host.primary_network_interface[0].id
 }
 
-data "ibm_is_instance_network_interface" "jump-001-nic" {
-    instance_name = ibm_is_instance.jump-001.name
-    network_interface_name = ibm_is_instance.jump-001.primary_network_interface[0].name
+data "ibm_is_instance_network_interface" "jump-host-nic" {
+    instance_name = ibm_is_instance.jump-host.name
+    network_interface_name = ibm_is_instance.jump-host.primary_network_interface[0].name
 }
 
 resource "ibm_is_security_group_rule" "allow-ssh" {
-  group     = data.ibm_is_instance_network_interface.jump-001-nic.security_groups[0].id
+  group     = data.ibm_is_instance_network_interface.jump-host.security_groups[0].id
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp {
