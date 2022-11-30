d49d9ae6b36f0f4b35c96a88a890b49f9d7ec7a8
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-11-30 15:20:24 +0530

    add jump-host cloud-init initial config

--- jump-host.tf
+++ jump-host.tf
@@ -11,6 +11,21 @@ resource "ibm_is_instance" "jump-host" {
   zone           = var.zone
   keys           = [data.ibm_is_ssh_key.iresh-pc.id]
   resource_group = ibm_resource_group.VMware.id
+  user_data      = <<EOT
+                #cloud-config
+                hostname: jump-host
+                fqdn: jump-host.vmware.ibmcloud.local
+                users:
+                  - default
+                  - name: iresh
+                    gecos: Iresh Dissanayaka
+                    groups: wheel
+                    shell: /bin/bash
+                    plain_text_passwd: admin123
+                    lock_passwd: false
+                    ssh_authorized_keys:
+                      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC64dK7V6GBPV77TzGbsMlEKvZh0earMCCYL7lSseqpLECLVHrO7AjdDjM8jMGezPD4sHGAJN105ZZdu887M4+7KufuUF/CooVJ3e7iH/aL98LZBXxgbsebxvPJael2MULXO6koK26QHrY9FwtVjc1BQWG7MbCaCH3CjhNa+nefEaBhGj+0jnd2eDZjUCTMzFt/H50Ref9tRcAiQCwG9KBJVKMrhiSrjiKcfowVFNbvBunlLFb0Gy79UHP4AVHMl3burUS9cZlU092lBvY+oyTZrKucgm8lGB+d8t9uiMfGGv9uYmka20hPd672N+qa/PdwESXFkd9QoOVkVp3FpqA40TNyeW0DKIk4BOnn3xVzgs0SXyXqF7W8a4rltR0GTqvQ93FC6kdTwlQUhXEjXdUMM/zNJRXALV0Dw/av4QgON18a4UoU2Xd8jxBsQDf7ENlWXdfavFn6KhI/iY3IaOyezfnzlj5qbQBLom7nqFJj1YZW1C6rK51oCf9q2bQPrF0= iresh@iresh-pc
+                EOT
 }
 
 resource "ibm_is_floating_ip" "jump-host-fip" {
