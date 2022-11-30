e0a558a58787f687342fa2705b0634fe4a14ae48
Iresh M Dissanayaka <socialexpz1@gmail.com>
2022-11-30 15:17:38 +0530

    change default cloud regioin to us-south

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "zone" {
     description = "IBM Cloud zone"
-    default = "eu-de-2"
+    default = "us-south-1"
 }
 
 variable "region" {
   description = "IBM Cloud region"
-  default = "eu-de"
+  default = "us-south"
 }
